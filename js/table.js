d56f899effd3a242e58831165c0d1b3661e7f1ca
Lok <1027408923@qq.com>
2025-04-16 21:50:29 +0800

    April 16 second save

---
+++ js/table.js
@@ -0,0 +1,7 @@
+// alert("succeed!")
+// function for adding new data
+function addRow(){
+    var table=document.getElementById("table1");
+    var length=table.rows.length();
+    console.log(length);
+} 
